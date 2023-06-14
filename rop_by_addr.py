1e8c4a7dc22a5143b46aa5e1998846d00e57abb2
msh1307 <86157260+msh1307@users.noreply.github.com>
2023-06-15 00:14:08 +0900

    Create rop_by_addr.py

---
+++ rop_by_addr.py
@@ -0,0 +1,144 @@
+import gdb
+from capstone import *
+from tqdm import tqdm
+import pickle
+
+default = "./results.rop"
+def brief(x,keywords):
+    m = 0x7ffffffff
+    M = -1
+    Ml = 0
+    for i in keywords:
+        if m > x.index(i):
+            m = x.index(i)
+        if M < x.index(i):
+            M = x.index(i)
+            Ml = len(i)
+    if m == -1 or M == -1:
+        return None
+    s,S = 0,0
+    if M+Ml+1 > len(x):
+        S = 1
+    if m-21 < 0:
+        s = 1
+    if s and not S:
+        return x[:M+Ml+1]
+    elif not s and S:
+        return x[m-21:]
+    elif not s and not S:
+        return x[m-21:M+Ml+1]
+    else:
+        return x
+    
+def parse_int(v):
+    if v.startswith("0x"):
+        v = int(v,16)
+    else:
+        v = int(v,10)
+    return v
+def search(s,sv):
+    if sv:
+        print("save? (y/n) : ",end='')
+        v = input() == 'y'
+        if v: 
+            with open(default,"wb") as f:
+                pickle.dump(s, f)
+            print("saved")
+    print()
+    print("Examples)\n\t1) array search :\n\t\tSearch > ['xchg','esp','ret']\n\t2) string search :\n\t\tSearch > xchg esp\n\t3) quit :\n\t\tSearch > q\n\t4) Save results :\n\t\tSearch > save")
+    print()
+    while True:
+        print("Search > ",end='')
+        v = input()
+        print("limit : ",end='')
+        if v == 'q':
+            break
+        elif v.startswith('['):
+            limit = parse_int(input())
+            arr = eval(v)
+            res = []
+            for i in s:
+                f = 0
+                cur = -1
+                idx = []
+                for j in arr:
+                    if j not in i:
+                        f = 1
+                        break
+                    else:
+                        if cur < i.index(j):
+                            idx.append(i.index(j))
+                            cur = i.index(j)
+                        else:
+                            f = 1
+                if f==0:
+                    m = sum(idx) / len(idx)
+                    M = 0
+                    t = 0
+                    for k in idx:
+                        t = (m - k)**2
+                        if M < t:
+                            M = t
+                    res.append([t,i])
+            print("show brief (y/n) : ",end= '')
+            v = input() == 'y'
+            res.sort()
+            for i in range(limit):
+                if v:
+                    x= brief((res[i][1]),arr)
+                    if x == None:
+                        continue
+                    print(x)
+                else:
+                    print((res[i][1]))
+        else:
+            limit = parse_int(input())
+            for i in s:
+                if v in i:
+                    print(i)
+
+if __name__ == '__main__':
+    print("load? (y/n) : ",end='')
+    v = input() == 'y'
+    if v:
+        try:
+            s = ''
+            with open(default,'rb') as f:
+                s = pickle.load(f)
+            print("loaded successfully")
+            search(s,False)
+        except FileNotFoundError:
+            print("'results.txt' not found")
+    else:
+        inf = gdb.inferiors()[0]
+        print("segment address : ",end='')
+        addr = parse_int(input())
+        res = gdb.execute(f"xinfo {addr}",to_string=True)
+        if 'Containing mapping:' in res:
+            res = res[res.index('0x')+2:]
+            res = res[res.index('0x'):].split()
+            print(f"reading memory {res[0]} ~ {res[1]} (0x{res[3]} bytes)")
+            mem = (inf.read_memory(int(res[0],16),int(res[3],16))).tobytes()
+            md = Cs(CS_ARCH_X86, CS_MODE_64)
+            gadgets = [b'\xc3',b"\xc2",b'\xcb',b"\xca",b'\xf2\xc3',b"\xf2\xc2",b'\xff',b'\xeb',b'\xe9',b'\xf2\xff',b'\xcd\x80',b"\x0f\x34",b"\x0f\x05",b'\x65\xff\x15\x10\x00\x00\x00']
+            candi = []
+            for i,j in tqdm(enumerate(mem)):
+                if j == gadgets[0][0]:
+                    candi.append(i)
+            s = []
+            base = int(res[0],16)
+            # print("width : ",end='')
+            # v = parse_int(input())
+            v = 0x20 # width 0x20 by default
+            for j in candi:
+                tmp = ''
+                if j-v < 0:
+                    for i in md.disasm(mem[:j+v], v):
+                        tmp += ("%s:\t%s %s\n" %('0x'+hex(i.address + base+j-v).replace('0x','').zfill(16), i.mnemonic, i.op_str))
+                else:
+                    for i in md.disasm(mem[j-v:j+v], v):
+                        tmp += ("%s:\t%s %s\n" %('0x'+hex(i.address + base+j-v).replace('0x','').zfill(16), i.mnemonic, i.op_str))
+                s.append(tmp)
+            search(s,True)
+        else:
+            print("not mapped")
